2dac352f2c9f4dfb341fd6dff18ad315761cd9f3
thekirici <83590661+thekirici@users.noreply.github.com>
2021-05-25 18:09:24 +0300

    Add files via upload

---
+++ coursera-4/script.js
@@ -0,0 +1,17 @@
+(function(){
+
+ var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+ for (var name in names) {
+
+       var firstLetter = names[name].charAt().toLowerCase();
+
+       if (firstLetter === "j") {
+
+          byeSpeaker.speak(names[name]);
+    } else {
+
+          helloSpeaker.speak(names[name]);
+  }
+}
+})();
